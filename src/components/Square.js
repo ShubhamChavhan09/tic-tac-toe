f863fb114a9af0f714ec68502b66ff2f4b7dcba2
Shubham <shubh.chavhan00@gmail.com>
2022-01-10 18:02:51 +0530

    added Square component

---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Square = ({ value, onClick }) => {
+  return <Button onClick={onClick}>{value}</Button>;
+};
+
+export default Square;
+const Button = styled.button`
+  cursor: pointer;
+  padding: 5px;
+`;
