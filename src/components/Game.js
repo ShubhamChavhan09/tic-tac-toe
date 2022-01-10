748edc36833db2f32974c52c5e95dc465b341312
Shubham <shubh.chavhan00@gmail.com>
2022-01-10 20:52:41 +0530

    added state for the app

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./Board";
+import { calculateWinner } from "../helpers";
 
 const Game = () => {
-  const handleClick = () => {};
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(board);
+
+  const handleClick = (index) => {
+    const tempBoard = [...board];
+    // check if the item is clicked or if we have a  winner
+    if (winner || tempBoard[index]) return;
+
+    //
+    tempBoard[index] = xIsNext ? "X" : "O";
+    setBoard(tempBoard);
+    setXIsNext(!xIsNext);
+  };
+
+  const handleReset = () => {
+    setBoard(Array(9).fill(null));
+  };
+
   return (
     <div>
-      <Board onClick={handleClick} />
+      <Board squares={board} onClick={handleClick} />
+      <div>
+        {winner ? `${winner} is the Winner` : `Next: ${xIsNext ? "X" : "O"}`}
+      </div>
+      <button onClick={handleReset}>Restart Game</button>
     </div>
   );
 };
